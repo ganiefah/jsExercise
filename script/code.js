ce97ea7c2987e49b5c5fe3c82046b67c2abdd1a5
ganiefah <ganiefahmajiet3@gmail.com>
2023-05-26 08:28:07 +0200

    done

--- script/code.js
+++ script/code.js
@@ -66,3 +66,19 @@ console.log(address);
 
 //q3 start
 
+const numb1 = document.querySelector('#numb1')
+const op = document.querySelector('#operator')
+const numb2 = document.querySelector('#Numb2')
+const answer = document.querySelector('#result')
+const button = document.querySelector('.btn')
+
+
+button.addEventListener('click',()=>{
+    let method = eval(numb1.value + op.value + numb2.value)
+
+    if(numb1,op,numb2 == ''){
+        numb1,op,numb2 === ''
+    }else{
+        answer.innerHTML = method
+    }
+})
